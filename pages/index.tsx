abb06d0374fbac2c0844fc221f84c2cbbb2dad11
Richard Essuman <ressuman001@gmail.com>
2024-11-05 10:58:55 +0000

    "Added React state and textarea input field to Home component"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
 
 export default function Home() {
+  const [input, setInput] = useState("");
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,10 +19,29 @@ export default function Home() {
       </Head>
 
       <div className="max-w-7xl mx-auto py-12">
-        <h1 className="text-2xl font-b  old text-center pb-2">
+        <h1 className="text-2xl font-bold text-center pb-2">
           AI Marketing Generator
         </h1>
-        {/* Input field for marketing copy */}
+        {/* Input field for marketing  */}
+        <div className="flex flex-col gap-4 justify-center w-1/3 mx-auto">
+          <div className="relative w-full">
+            <textarea
+              rows={3}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="w-full border-2 border-gray-300 bg-white p-4 rounded-lg text-sm focus:outline-none resize-none"
+              placeholder="Enter your topic here"
+            />
+            {/* Character limit in bottom right of textarea */}
+            <div
+              className={`absolute
+              ${input.length > 100 ? "text-red-500" : "text-gray-400"}
+              bottom-2 right-2 text-xs`}
+            >
+              <span>{input.length}</span>/100
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
