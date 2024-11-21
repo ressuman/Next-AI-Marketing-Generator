9e3dec5f71850d30c3bace243d0648a997c3c7ce
Richard Essuman <ressuman001@gmail.com>
2024-11-21 13:01:23 +0000

    Add react-spinners for loading state; implement character limit validation and error handling in Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
+import { MoonLoader } from "react-spinners";
 
 export default function Home() {
   const [input, setInput] = useState("");
-  //const [error, setError] = useState(false);
-  //const [suggestion, setSuggestion] = useState("");
-  //const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [suggestion, setSuggestion] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (input.length <= 50) setError(false);
+  }, [input]);
 
   const submit = async () => {
     // Check if character limit is exceeded
-    //if (input.length > 100) return setError(true);
+    if (input.length > 50) return setError(true);
 
     // Set loading state
-    //setLoading(true);
+    setLoading(true);
 
     try {
       const res = await fetch("/api/marketing-ai", {
@@ -24,16 +29,15 @@ export default function Home() {
         body: JSON.stringify({ input }),
       });
 
-      //const suggestion: { result: string } = await res.json();
-      //const { result } = suggestion;
-      //console.log("result", result);
-      const suggestion: string = await res.json();
-      console.log(suggestion);
-      //setSuggestion(result);
+      const suggestion: { result: string } = await res.json();
+      const { result } = suggestion;
+      console.log("result", result);
+
+      setSuggestion(result);
     } catch (error) {
       console.log(error);
     } finally {
-      //setLoading(false);
+      setLoading(false);
     }
   };
 
@@ -54,9 +58,16 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-center pb-2">
           AI Marketing Generator
         </h1>
+
         {/* Input field for marketing  */}
         <div className="flex flex-col gap-4 justify-center w-1/3 mx-auto">
           <div className="relative w-full">
+            {/* Error message */}
+            {error && (
+              <p className="text-xs pt-1 text-red-500">
+                Character limit exceeded, please enter less text
+              </p>
+            )}
             <textarea
               rows={3}
               value={input}
@@ -67,10 +78,10 @@ export default function Home() {
             {/* Character limit in bottom right of textarea */}
             <div
               className={`absolute
-              ${input.length > 100 ? "text-red-500" : "text-gray-400"}
+              ${input.length > 50 ? "text-red-500" : "text-gray-400"}
               bottom-2 right-2 text-xs`}
             >
-              <span>{input.length}</span>/100
+              <span>{input.length}</span>/50
             </div>
           </div>
           <button
@@ -78,8 +89,27 @@ export default function Home() {
             onClick={submit}
             className="bg-blue-500 h-12 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Generate
+            {loading ? (
+              <div className="flex justify-center items-center gap-4">
+                <p>Loading...</p>
+                <MoonLoader size={20} color="white" />
+              </div>
+            ) : (
+              "Generate"
+            )}
           </button>
+
+          {/* Output field for marketing copy */}
+          {suggestion && (
+            <div className="mt-8">
+              <h4 className="text-lg font-semibold pb-2">
+                Your marketing copy:
+              </h4>
+              <div className="relative w-full rounded-md bg-gray-100 p-4">
+                <p className="text-sm text-gray-700">{suggestion}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
